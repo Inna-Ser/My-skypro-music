c92c8eeb29b6693fb77657f57498a2139adbe879
Inna-Ser <serebrakovainna1@gmail.com>
2024-03-18 21:06:46 -0500

    hw-6 fixed

--- src/components/audioplayer/progressbar/Progressbar.jsx
+++ src/components/audioplayer/progressbar/Progressbar.jsx
@@ -1,19 +1,40 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./Progressbar.module.css";
 
 export function ProgressBar({ audioRef }) {
   const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
   const progressbarRef = useRef(null);
 
   const changeTiming = (e) => {
-    audioRef.current.currentTime = e.target.value;
-    setCurrentTime(audioRef.current.currentTime);
+    const { value } = e.target;
+    audioRef.current.currentTime = value;
+    setCurrentTime(value);
   };
 
-  audioRef.current &&
-    audioRef.current.addEventListener("timeupdate", () => {
+  useEffect(() => {
+    const updateTime = () => {
       setCurrentTime(audioRef.current.currentTime);
-    });
+    };
+    if (audioRef.current) {
+      setDuration(audioRef.current.duration);
+      audioRef.current.addEventListener("timeupdate", updateTime);
+    }
+
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.removeEventListener("timeupdate", updateTime);
+      }
+    };
+  }, [audioRef]);
+
+  useEffect(() => {
+    if (progressbarRef.current) {
+      const progressBar = progressbarRef.current;
+      const percentPlayed = (currentTime / duration) * 100;
+      progressBar.style.backgroundSize = `${percentPlayed}% 100%`;
+    }
+  }, [currentTime, duration]);
 
   return (
     <input
@@ -21,6 +42,7 @@ export function ProgressBar({ audioRef }) {
       ref={progressbarRef}
       type="range"
       min={0}
+      max={duration}
       value={currentTime}
       step={0.01}
       onChange={changeTiming}
